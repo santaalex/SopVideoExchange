8999de1793f48d1a52f1cac412db17180b13df7a
SantaAlex <santaalex@github.com>
2026-02-19 23:58:41 +0800

    fix: 移动页面到根目录适配 Next.js 标准结构

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
       bodySizeLimit: '500mb',
     },
   },
+  // 支持 src 目录
+  srcDir: 'src/',
+  // 支持 presentation/pages 作为页面目录
+  pageExtensions: ['ts', 'tsx'],
 }
 
 module.exports = nextConfig
